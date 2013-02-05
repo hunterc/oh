7697588d31c8c176c7206ad57eb65bf281eb35b1
dagrami <dgm3df@virginia.edu>
2013-02-05 12:54:28 -0500

    fixed db login

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 	class DbUtil{
-		public static $lu = "cs4720hwc2d";
-		public static $lp = "hwc8591";
+		public static $lu = "ohqueue";
+		public static $lp = "backupdb";
 		public static $host = "stardock.cs.virginia.edu";
-		public static $schema = "cs4720hwc2d";
+		public static $schema = "ohqueue";
 		
 		public static function loginConnection() {
 			$db = new mysqli(DbUtil::$host, DbUtil::$lu, DbUtil::$lp, DbUtil::$schema);
