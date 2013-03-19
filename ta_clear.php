10b5884d192b2ba016e3209d1ce7ff9d2397211b
Hunter Cassidy <hwc2d@virginia.edu>
2013-03-19 11:26:50 -0300

    Update ta_clear.php

--- ta_clear.php
+++ ta_clear.php
@@ -1,10 +1,19 @@
 <?php
-
+  $comp_id = $_SERVER['PHP_AUTH_USER'];
   require_once('dbconnect.php');
-
+  
   $db = DbUtil::loginConnection();
   $stmt = $db -> stmt_init();
 
+  if($stmt -> prepare('SELECT role FROM roster WHERE comp_id = ?') or die(mysqli_error($db))) {
+  	$stmt -> bind_param("s", $comp_id);
+		$stmt -> execute();
+		$stmt -> bind_result($user_role);
+		$stmt -> fetch();
+	}
+  
+  if($user_role == 'Instructor' || $user_role == 'TA'){
+  
   if($stmt -> prepare('DELETE FROM active_queue') or die (mysqli_error($db))) {
     $stmt -> execute();
     $db -> commit();
@@ -16,5 +25,8 @@
 
   echo '<button id="close_alert" class="close">×</button>';
   echo 'Successfully removed all students from queue.';
-  
+  } else {
+    header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found', true, 404);
+    exit;
+  }
 ?>
